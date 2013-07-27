72cef61c11ee8eb2b06dabb2dc4707a8a3f28f56
Alexey Novak <abovebluesky@gmail.com>
2013-07-27 14:58:08 -0400

    grunt: Added grunt task for LESS and also for copying necessary images into diet/ folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,32 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['!assets/imgs/image.jpg', 'assets/imgs/*'], flatten: true, dest: 'dist/imgs/', filter: 'isFile'} // includes files in path
+        ]
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: ["css"]
+        },
+        files: {
+          "dist/css/<%= pkg.name %>.css": "assets/a11yAccordeon.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ["css"],
+          yuicompress: true
+        },
+        files: {
+          "dist/css/<%= pkg.name %>.css": "assets/a11yAccordeon.less"
+        }
+      }
+    },
     concat: {
       options: {
         separator: ';'
@@ -12,7 +38,7 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= pkg.version %> / Last build on: <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> / Last build on: <%= grunt.template.today("dd-mm-yyyy") %> / This file is under <%= pkg.version %> license. */\n'
       },
       my_target: {
         files: {
@@ -24,6 +50,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['copy', 'less', 'concat', 'uglify']);
 };
