dbdfe3a174c7d6ffd8143d146a7e82ff1a646ed3
Alexey Novak <abovebluesky@gmail.com>
2013-11-29 13:02:22 -0500

    tests: Added a skeleton for future a11yAccordeon tests

---
+++ tests/js/a11yAccordeon-tests.js
@@ -0,0 +1,31 @@
+/*global describe, afterEach, it, chai, a11yAccordeon*/
+
+(function (expect) {
+  describe("a11yAccordeon tests", function() {
+
+    afterEach(function () {
+      // do some other cleanups here
+    });
+
+    it("a11yAccordeon creation tests", function() {
+      expect(true).to.be.ok;
+    });
+
+    it("collapseRow() tests", function() {
+      expect(true).to.be.ok;
+    });
+
+    it("uncollapseRow() tests", function() {
+      expect(true).to.be.ok;
+    });
+
+    it("toggleRow() tests", function() {
+      expect(true).to.be.ok;
+    });
+
+    it("getRowEl() tests", function() {
+      expect(true).to.be.ok;
+    });
+
+  });
+})(chai.expect);
